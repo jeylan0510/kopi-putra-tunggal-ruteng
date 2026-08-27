d98e1025bac51e9cc59c2742b5a09449438d7406
unknown <vianjany182@gmail.com>
2026-08-27 12:52:42 +0800

    Set default APP_KEY to resolve Laravel 500 error on Railway

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY') ?: 'base64:JEYLAN0510KOPIPUTRATUNGGALRUTENG2026=',
 
     'previous_keys' => [
         ...array_filter(
